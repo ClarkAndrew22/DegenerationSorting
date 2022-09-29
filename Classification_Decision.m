1034540e973cf5d8418fd3344500650c2cd6296a
andrewclark <andrewclark@andrews-mbp.lan>
2022-09-29 18:22:31 -0400

    Added total degeneration matrix.

--- Classification_Decision.m
+++ Classification_Decision.m
@@ -22,11 +22,47 @@ meanBreak = mean(dendriteBreakProp,2);
 meanLength = mean(dendriteLengths,2);
 meanIntensity = mean(dendriteIntensities,2);
 
+% Allocate for speed
+featureCount = zeros(numel(imgSize),1);
+weightFeatureCount = zeros(numel(imgSize),1);
+featureCountLen = zeros(numel(imgSize),1);
+meanSize = zeros(numel(imgSize),1);
+minSize = zeros(numel(imgSize),1);
+maxSize = zeros(numel(imgSize),1);
+
 % Get bleb data on per image basis using for loop
 for img = 1:numel(imgSize)
 
     tempTable = MasterFeatureTable(MasterFeatureTable.ImageNumber == img, :);
-    
 
+    % Get features
+    featureCount(img) = size(tempTable,1);
+    weightFeatureCount(img) = featureCount(img)./meanBreak(img);
+    featureCountLen(img) = featureCount(img)./meanLength(img);
+
+    if ~isempty(tempTable)
+        meanSize(img) = mean(tempTable.Area);
+        minSize(img) = min(tempTable.Area);
+        maxSize(img) = max(tempTable.Area);
+    else
+        minSize(img) = NaN;
+        maxSize(img) = NaN;
+    end
 
 end
+
+% Combine all values
+totalDegen = [meanBreak, featureCount, weightFeatureCount, meanSize];
+
+
+
+
+
+
+
+
+
+
+
+
+
